2ddcf97dbf0700bc4d493275f1762100a3806292
Anne Schumacher <anne.schumacher@gu.se>
2018-04-13 16:58:16 +0200

    moved html

--- md2html.sh
+++ md2html.sh
@@ -1,11 +1,13 @@
 # Convert all markdown files ending in md to html
 # for file in ./*.md; do
-# 	markdown $file > "`basename $file .md`.html"
+# 	markdown $file > "html/`basename $file .md`.html"
 # done
 
 for file in ./*.md; do
-    gpp -H -DHTML=1 --include macros.gpp $file | pandoc -o `basename $file .md`.html \
+    gpp -H -DHTML=1 --include macros.gpp $file | pandoc -o html/`basename $file .md`.html \
     --listings `# use listings package for LaTeX code blocks` \
     -V urlcolor=RoyalBlue `# color links blue` \
     -V links-as-notes=true `# print links as footnotes`
 done
+
+rm html/README.html
